54a82b7bdb91b792dfce3a2f3330d1af359fc56b
Ashley Buchanan <ashleyshanebuchanan@gmail.com>
2025-11-14 17:00:20 -0800

    i just had to rewrite all of it. i wasnt pleased with what i had.

--- step3.js
+++ step3.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
 const axios = require('axios');
-const open = (...args) => import('open').then(mod => mod.default(...args));         // the open package is ESM-only... grrr
+const open = (...args) => import('open').then(mod => mod.default(...args));         // the open package is ESM-only... I could've just converted to ESM but I'd already struggled to find this.:D
 
-function cat(path) {
+function output(content, outPath) {
+    if (outPath) {
+        try {
+            fs.writeFileSync(outPath, content, 'utf8');
+        } catch (err) {
+            console.error(`Couldn't write ${outPath}: ${err.message}`);
+            process.exit(1)
+        }
+    } else {
+        console.log(content);
+    }
+}
+
+function cat(path, outPath) {
     try {
         const data = fs.readFileSync(path, 'utf8');
-        console.log(data);
+        console.log(data, outPath);
     } catch (err) {
         console.error(`Error reading ${path}:`, err.message);
         process.exit(1);
@@ -26,31 +39,84 @@ async function validated(url) {
     }
 }
 
-async function webCat(url) {
+async function webCat(src, dest) {
+    let payload;
+
     try {
-        await open(url);
+        payload = await axios.get(src);
     } catch (err) {
-        console.error(`Error opening ${url}:`, err.message);
+        console.error(`Error fetching ${src}:`, err);
         process.exit(1);
+    }
+
+    switch(dest) {
+        case 'console':
+            console.log(payload.data);
+            break;
+        
+        case 'browser':
+            await open(src);
+            console.log(`${src} passed to default browser.`);
+            break;
+
+        default:
+            //write to file
+            break;
     }
 }
 
 async function main() {
-    const arg = process.argv[2];
-    if (!arg) {
-        console.error('Please provide a file path.');
-        process.exit(1);
-    }
+    const args = process.argv.slice(2);
+    console.log(args, args.length);
 
-    if (arg.startsWith('http://') || arg.startsWith('https://')) {
-        if (await validated(arg)) {
-            await webCat(arg);
-        }
-    } else {
-        cat(arg);
-    }
+    //if no args[]:
+    if (args.length === 0) {
+        console.error('Please provide a file path or web address.');
+        console.log('\nExample Usage:');
+        console.log(' node step3.js [--out] <file path or web address> <file destination, [browser], or [console]>\n\n');
+        process.exit(1);
+    };
+
+
+    //if --out and destination specified:
+    if (args[0] === '--out' && args[2]) {
+        webCat(args[1], args[2]);
+    
+    //if --out and no destination specified:
+    } else if (args[0] === '--out') {
+        webCat(args[1], 'console'); 
+
+    //if no--out and destination specified:
+    } else if (args[0] !== '--out' && args[1]) {
+        webCat(args[0], args[1]);
+
+    //if no--out and no destination specified:
+    } else if (args[0] !== '--out') {
+        webCat(args[0], 'console');
+    };
+
+
+
+    
+    
+
+    
+    
+
+
+
+
+
+
+    // if (args[2].startsWith('http://') || args[2].startsWith('https://')) {
+    //     if (await validated(args)) {
+    //         await webCat(args);
+    //     }
+    // } else {
+    //     cat(args);
+    // }
 }
 
 main();
 
-// i wanted to provide a quick check before opening the url, so I duct taped both axios and open to accomplish this.
+// i wanted to provide a quick check before opening the url, so I duct-taped both axios and open to accomplish this.
